5d65a4cb63234ffb9ebb2dde3394bc3779ad4f56
Diego Ugarte <Diego.wtf2@gmail.com>
2018-06-04 22:27:15 -0400

    Se divide el index por secciones

--- cybertesis/tesis/local_settings.py
+++ cybertesis/tesis/local_settings.py
@@ -1,12 +1,2 @@
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cybertesis',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
 
-}
